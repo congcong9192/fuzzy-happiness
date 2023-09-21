0c2642f5592720ad1a20123aa4c6d032b5e3fb4e
congcong9192 <97867923+congcong9192@users.noreply.github.com>
2023-09-22 01:33:34 +0800

    Update settings.py

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rc@04_mry_3-$@2sq$b9%-9jp6q2eyxf4bsw9&&esj++aw&r)p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
 
 
 # Application definition
