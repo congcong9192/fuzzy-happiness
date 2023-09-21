2d3f017a66f29b01dac3f6caca2427a179d877f8
congcong9192 <97867923+congcong9192@users.noreply.github.com>
2023-09-22 01:29:44 +0800

    Update settings.py

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -27,10 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-if 'CODESPACE_NAME' in os.environ:
-    codespace_name = os.getenv("CODESPACE_NAME")
-    codespace_domain = os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
-    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}']
 
 # Application definition
 
